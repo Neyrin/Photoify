2ac105f8a858aa8ea2a6be76d6b0c69dc5f1d4fa
Neyrin <isa.areschoug@gmail.com>
2018-12-24 11:00:26 +0100

    Created config.php

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+// Configuration settings.
+
+return [
+    'title' => 'Photoify',
+    'database_path' => sprintf('sqlite:%s/database/photoify.db', __DIR__),
+];
