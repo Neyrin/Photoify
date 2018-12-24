e065b23b550934d7d34b460a4356e5c494155826
Neyrin <isa.areschoug@gmail.com>
2018-12-24 10:56:50 +0100

    Created index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require __DIR__.'/views/header.php';
+
+?>
+
+<article>
+    <?php
+    /* If there's an active user redirect to home */
+    if(isset($_SESSION['user'])):  ?>
+    <?php require __DIR__.'/home.php'; ?>
+
+    <?php
+    /* If there's no active user redirect to start/login page */
+    else: ?>
+        <?php require __DIR__.'/start.php'; ?>
+    <?php endif; ?>
+</article>
+
+
+<?php require __DIR__.'/views/footer.php'; ?>
