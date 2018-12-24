b048317a33c8f7614f83a9a692c6402c745fea62
Neyrin <isa.areschoug@gmail.com>
2018-12-24 11:03:34 +0100

    Created header.php

---
+++ views/header.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__.'/../app/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Photoify</title>
+    <link rel="stylesheet" href="assets/style/main.css">
+    <link rel="stylesheet" href="assets/style/navbar.css">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans" rel="stylesheet">
+  </head>
+<body>
+    <div class="navbar"></div>
+    
