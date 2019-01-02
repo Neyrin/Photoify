0876a70cf23713bab2f3423c98de80b2f3d74884
Neyrin <isa.areschoug@gmail.com>
2019-01-02 21:24:01 +0100

    Updated header.php

--- views/header.php
+++ views/header.php
@@ -11,6 +11,7 @@ require __DIR__.'/../app/autoload.php';
     <title>Photoify</title>
     <link rel="stylesheet" href="assets/style/main.css">
     <link rel="stylesheet" href="assets/style/navbar.css">
+    <link rel="stylesheet" href="assets/style/start.css">
     <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
   </head>
