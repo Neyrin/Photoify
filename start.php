edc70042f266b9c6a425fe0eb9a106d557c05b4d
Neyrin <isa.areschoug@gmail.com>
2019-01-02 21:24:51 +0100

    Updated start.php

--- start.php
+++ start.php
@@ -1,20 +1,20 @@
 <article>
 
 <form class="sign-in" action="app/users/login.php" method="post">
-    <h1>Sign in</h1>
+    <h1>SIGN IN</h1>
     <div class="form-group">
-        <label for="email">Email</label> <br>
-        <input class="form-control" type="email" name="email" placeholder="Enter your email" required>
+        <label for="email">EMAIL</label> <br>
+        <input class="form-login" type="email" name="email" placeholder="ENTER YOUR EMAIL" required>
     </div>
 
     <div class="form-group">
-        <label for="password">Password</label><br>
-        <input class="form-control" type="password" name="password" required>
+        <label for="password">PASSWORD</label><br>
+        <input class="form-login" type="password" name="password" required>
     </div>
 
-    <button type="submit" class="login-button">Sign in</button>
+    <button type="submit" class="login-btn">SIGN IN</button>
 
-    <p>Not a member yet? Sign up <a href="signup.php">here</a>!</p>
+    <p>NOT A MEMBER YET? SIGN UP <a href="signup.php">HERE</a>!</p>
     
 </form>
 </article>
