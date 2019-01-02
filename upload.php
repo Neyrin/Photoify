c90acd899fb65cde574773a292b74cd2353d72ad
Neyrin <isa.areschoug@gmail.com>
2019-01-02 21:25:10 +0100

    Updated upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,9 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<form action="" method="post" enctype="multipart/form-data">
+<form action="app/posts/upload-post.php" method="post" enctype="multipart/form-data">
     <input type="file" name="image" id="image" multiple>
-    <input type="text" name="caption" placeholder="Why not give your image a caption?">
-    <button type="submit" name="uploadbtn">Upload</button>
+    <input type="text" name="caption" placeholder="WHY NOT GIVE YOUR IMAGE A CAPTION?">
+    <button type="submit" name="uploadBtn">UPLOAD</button>
 </form>
 
 <?php require __DIR__.'/views/footer.php'; ?>
