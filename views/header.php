985843d2d95730e5f7311adcd2cabe408f12b8e7
Neyrin <isa.areschoug@gmail.com>
2019-01-02 11:24:55 +0100

    Worked on navbar

--- views/header.php
+++ views/header.php
@@ -12,7 +12,17 @@ require __DIR__.'/../app/autoload.php';
     <link rel="stylesheet" href="assets/style/main.css">
     <link rel="stylesheet" href="assets/style/navbar.css">
     <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/solid.css" integrity="sha384-+0VIRx+yz1WBcCTXBkVQYIBVNEFH1eP6Zknm16roZCyeNg2maWEpk/l/KsyFKs7G" crossorigin="anonymous">
   </head>
 <body>
-    <div class="navbar"></div>
-    
+    <div class="navbar">
+      <?php if(isset($_SESSION['user'])): ?>
+      <button class="btn" id="home_btn"><i class="fas fa-home"></i></button>
+      <button class="btn" id="upload_btn"><i class="fas fa-camera-retro"></i></button>
+      <button class="btn" id="user_btn"><i class="fas fa-user-circle"></i></button> 
+    <?php
+    else: ?> 
+      <p> Waddup? </p>
+    <?php endif; ?>
+    </div>
+   
