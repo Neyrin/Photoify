c696a5e1abf5735835394e5aa4826e8a8e69e91c
Neyrin <isa.areschoug@gmail.com>
2019-01-04 14:20:50 +0100

    Started working on preview of image before upload

--- upload.php
+++ upload.php
@@ -1,9 +1,11 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<form action="app/posts/upload-post.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="image" id="image" multiple>
-    <input type="text" name="caption" placeholder="WHY NOT GIVE YOUR IMAGE A CAPTION?">
-    <button type="submit" name="uploadBtn">UPLOAD</button>
+<form class="upload-post" action="app/posts/upload-post.php" method="post" enctype="multipart/form-data">
+<!--     <img id="preview" />
+    <button id="select-btn">SELECT IMAGE</button> -->
+    <input class="form-upload" type="file" name="image" id="image" multiple>
+    <input class="form-upload" type="text" name="caption" placeholder="WHY NOT GIVE YOUR IMAGE A CAPTION?">
+    <button class="upload-btn" type="submit" name="upload-btn">UPLOAD</button>
 </form>
 
 <?php require __DIR__.'/views/footer.php'; ?>
