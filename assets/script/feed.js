b518ed11cb0b5246fc4a069242e2e727c9668aba
Neyrin <isa.areschoug@gmail.com>
2019-01-04 19:14:00 +0100

    Created feed.js

---
+++ assets/script/feed.js
@@ -0,0 +1,19 @@
+'use strict'
+
+fetch('./app/users/feed.php')
+.then(response => response.json())
+.then(posts => { /* console.log(posts)); */  
+    posts.forEach(post => {
+        const content = document.createElement('img');
+        content.classList.add('post-image');
+        content.setAttribute("src", `${post.image}`);
+        
+        const caption = document.createElement('p');
+        caption.classList.add('post-caption');
+        caption.innerHTML = post.caption;
+        
+        const feed = document.getElementById("feed");
+            feed.appendChild(content);
+            feed.appendChild(caption); 
+        });
+    }); 
