cc7d4ac236d1a2031e0392245cf394b2ce8d8735
Neyrin <isa.areschoug@gmail.com>
2019-01-04 14:15:51 +0100

    Dynamic rendering for user information

---
+++ mypage.php
@@ -0,0 +1,11 @@
+<?php require __DIR__.'/views/header.php';?>
+
+<div class="container">
+    <div class="user-info" id="user-info">
+    </div> <!--/user-info-->
+    <div class="user-images">
+        </div><!--/user-images-->
+    </div><!--/container-->
+    
+<script src="assets/script/mypage.js"></script>
+<?php require __DIR__.'/views/footer.php'; ?>
