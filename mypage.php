22118bf9cadddea03fa1b969919ee8f15ac2b0d6
Neyrin <isa.areschoug@gmail.com>
2019-01-07 16:37:10 +0100

    Troubleshooting

--- mypage.php
+++ mypage.php
@@ -1,5 +1,7 @@
 <?php require __DIR__.'/views/header.php';?>
-
+<div class="header">
+    <button class="btn" id="settings_btn"><i class="fas fa-sliders-h"></i></button>
+</div><!--/header-->
 <div class="container">
     <div class="user-info" id="user-info">
     </div> <!--/user-info-->
