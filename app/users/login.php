37ac6fe3960815642774574d2febf330370dcfff
Neyrin <isa.areschoug@gmail.com>
2019-01-11 10:12:42 +0100

    Changed header to redirect function

--- app/users/login.php
+++ app/users/login.php
@@ -3,26 +3,24 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-    if (isset($_POST['email'], $_POST['password'])) {
-        $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-
-
-        $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
-        $statement->bindParam(':email', $email, PDO::PARAM_STR);
-        $statement->execute();
-
-        $user = $statement->fetch(PDO::FETCH_ASSOC);
-
-        if (!$user)
-        {
-            header('location: ../../start.php');
-        }
-
-        if (password_verify($_POST['password'], $user['password'])) {
-            unset($user['password']);
-            $_SESSION['user'] = $user;
-        }
+if (isset($_POST['email'], $_POST['password'])) {
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    
+    
+    $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
+    $statement->execute();
+    
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+    
+    if (!$user)
+    {
+        redirect('../../start.php');
     }
-
-header('location: /photoify/index.php');
-die();
+    
+    if (password_verify($_POST['password'], $user['password'])) {
+        unset($user['password']);
+        $_SESSION['user'] = $user;
+    }
+}
+redirect();
