ef34c4d7970ebeabfa1f3a5df783df3e1174efc0
Neyrin <isa.areschoug@gmail.com>
2019-01-11 09:54:13 +0100

    Added actually working icon to settings-button

--- mypage.php
+++ mypage.php
@@ -1,11 +1,11 @@
 <?php require __DIR__.'/views/header.php';?>
 <div class="header">
-    <button class="btn" id="settings_btn"><i class="fas fa-sliders-h"></i></button>
+    <button class="btn" id="settings_btn"><i class="fa fa-sliders"></i></button>
 </div><!--/header-->
 <div class="container">
     <div class="user-info" id="user-info">
     </div> <!--/user-info-->
-    <div class="user-images">
+    <div class="user-images" id="user-images">
         </div><!--/user-images-->
     </div><!--/container-->
     
