9ffa555d98e007756fde0aa5ac50223827161806
Neyrin <isa.areschoug@gmail.com>
2019-01-11 10:10:18 +0100

    Redid to get both info and posts

--- app/users/mypage.php
+++ app/users/mypage.php
@@ -3,30 +3,39 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-$stmt = $pdo->query('SELECT first_name, last_name, user_name, user_id, avatar, email, bio FROM Users WHERE user_id = :user_id');
-$stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-$stmt->execute();
+$stmtInfo = $pdo->query('SELECT 
+                        first_name, 
+                        last_name, 
+                        user_name, 
+                        user_id, 
+                        avatar, 
+                        email, 
+                        bio 
+                    FROM Users 
+                    WHERE user_id = :user_id');
+$stmtInfo->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$stmtInfo->execute();
 
-if (!$stmt) {
+if (!$stmtInfo) {
     die(var_dump($pdo->errorInfo()));
 }
 
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-header('Content-Type: application/json');
-echo json_encode($users); 
+$users = $stmtInfo->fetchAll(PDO::FETCH_ASSOC);
 
 
 //Split into separate files and call in functions?
-$stmt2 = $pdo->query('SELECT * FROM Posts WHERE user_id = :user_id');
-$stmt2->bindParam(':user_id', $user_id, PDO::PARAM_INT);
-$stmt2->execute();
+$stmtPosts = $pdo->query('SELECT * FROM Posts WHERE user_id = :user_id');
+$stmtPosts->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$stmtPosts->execute();
 
-if (!$stmt2) {
+if (!$stmtPosts) {
     die(var_dump($pdo->errorInfo()));
 }
 
-$userPosts = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+$userPosts = $stmtPosts->fetchAll(PDO::FETCH_ASSOC);
 
 header('Content-Type: application/json');
-echo json_encode($userPosts); 
+echo json_encode([
+    'users' => $users,
+    'userPosts' => $userPosts
+]);
