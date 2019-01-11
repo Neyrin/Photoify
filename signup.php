766126624eac3cc4d478ba5571007f84722815f1
Neyrin <isa.areschoug@gmail.com>
2019-01-11 09:55:55 +0100

    Added error-message functionallity

--- signup.php
+++ signup.php
@@ -1,5 +1,12 @@
 <?php require __DIR__.'/views/header.php';
 
+  if(isset($_SESSION['messages'])){
+    foreach($_SESSION['messages'] as $message){
+      echo $message;
+    }
+    unset($_SESSION['messages']);
+  }
+
 ?>
 
 <article>
