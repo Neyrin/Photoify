275f2c6f856f87988fae3f4f51681fad0252f491
Neyrin <isa.areschoug@gmail.com>
2019-01-13 22:40:22 +0100

    Updated

--- edit.php
+++ edit.php
@@ -3,10 +3,10 @@
 
 <img class="post-to-edit">
 <p class="caption-to-edit"></p>
-<form class="edit-post" action="app/posts/edit/edit-post.php" method="post">
+<form class="edit-post" action="/app/posts/edit/edit-post.php" method="post">
     <input class="form-upload" type="text" name="caption" placeholder="UPDATE YOUR CAPTION?">
     <button class="edit-post-btn" type="submit" name="edit-post-btn">EDIT</button>
 </form>
 
-<!-- <script src="assets/script/update.js"></script> -->
+<!-- <script src="assets/script/edit.js"></script> -->
 <?php require __DIR__.'/views/footer.php'; ?>
