a3e88676784ac7b593fd283d665ed28f7acfdb98
Neyrin <isa.areschoug@gmail.com>
2019-01-13 22:32:08 +0100

    Worked on updating posts, missing connection to user id

--- app/posts/edit/edit-post.php
+++ app/posts/edit/edit-post.php
@@ -3,18 +3,25 @@ declare(strict_types=1);
 
 require __DIR__.'/../../autoload.php';
 
-$post_id = $_GET['post_id'];
-if(isset($_POST['caption'])) {
-    $newCaption = trim(filter_var($_POST['caption'], FILTER_SANITIE_STRING)) ?? ' ';
-    $id = $_POST['post_id'];
-    print_r($id);
+$post_id = trim(filter_var($_GET['post_id'], FILTER_SANITIZE_NUMBER_INT));
+$post_user_id = trim(filter_var($_GET['user_id'], FILTER_SANITIZE_NUMBER_INT));
 
-    $stmt = $pdo->prepare('UPDATE Posts SET caption = :caption WHERE post_id = :post_id');
-    $stmt->bindParam(':caption', $newCaption, PDO::PARAM_STR);
-    $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+print_r($post_id);
+print_r($post_user_id);
 
-    $stmt->execute();
+
+/* if($post_user_id !== $user_id){
+    $_SESSION['messages'][] = "You don't have permission to edit this post.";
+    redirect('/');
 } else {
-    echo 'nothing is set';
+    if(isset($_POST['caption'])) {
+        $newCaption = trim(filter_var($_POST['caption'], FILTER_SANITIE_STRING)) ?? ' ';
+
+        $stmt = $pdo->prepare('UPDATE Posts SET caption = :caption WHERE post_id = :post_id');
+        $stmt->bindParam(':caption', $newCaption, PDO::PARAM_STR);
+        $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+
+        $stmt->execute();
+    } 
 }
-/* redirect(); */
+redirect(); */
