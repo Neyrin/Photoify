3d37c3de0efa14ba52a1f2b2bc833f340f4457d4
Neyrin <isa.areschoug@gmail.com>
2019-01-15 21:35:41 +0100

    Added commentary

--- app/posts/edit/delete-post.php
+++ app/posts/edit/delete-post.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 require __DIR__.'/../../autoload.php';
 
-
+//Get post id from form
+//Then delete image from db
 if(isset($_POST['selected-post-id'])) {
     $selectedPostId = filter_var($_POST['selected-post-id'], FILTER_SANITIZE_NUMBER_INT);
 
