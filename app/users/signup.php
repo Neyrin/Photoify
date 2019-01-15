b6fdcd1357385f75f96c495ee4ad0858a465b4dd
Neyrin <isa.areschoug@gmail.com>
2019-01-15 21:55:04 +0100

    Added commentary, trim password, renamed statement variables

--- app/users/signup.php
+++ app/users/signup.php
@@ -10,34 +10,32 @@ require __DIR__.'/../autoload.php';
             $lastName = trim(filter_var($_POST['last_name'], FILTER_SANITIZE_STRING));
             $email = trim(filter_var($_POST['email'], FILTER_SANITIZE_EMAIL));
             $userName = trim(filter_var($_POST['user_name'], FILTER_SANITIZE_STRING));
-            //TRIM THE PASOWORD
-            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+            $password = trim(password_hash($_POST['password'], PASSWORD_DEFAULT));
 
-            /* $query = 'INSERT INTO users(first_name, last_name, email, user_name, password)
-            VALUES (:first_name, :last_name, :email, :user_name, :password)' */
-            $statement = $pdo->prepare('INSERT INTO users(first_name, last_name, email, user_name, password)
+            //Insert info into db
+            $stmtCreate = $pdo->prepare('INSERT INTO users(first_name, last_name, email, user_name, password)
             VALUES (:first_name, :last_name, :email, :user_name, :password)');
 
-            if (!$statement)
+            if (!$stmtCreate)
             {
                 die(var_dump($pdo->errorInfo()));
             }
 
-            // binds variables to parameteres for insert statement
-            $statement->bindParam(':first_name', $firstName, PDO::PARAM_STR);
-            $statement->bindParam(':last_name', $lastName, PDO::PARAM_STR);
-            $statement->bindParam(':email', $email, PDO::PARAM_STR);
-            $statement->bindParam(':user_name', $userName, PDO::PARAM_STR);
-            $statement->bindParam(':password', $password, PDO::PARAM_STR);
+            $stmtCreate->bindParam(':first_name', $firstName, PDO::PARAM_STR);
+            $stmtCreate->bindParam(':last_name', $lastName, PDO::PARAM_STR);
+            $stmtCreate->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmtCreate->bindParam(':user_name', $userName, PDO::PARAM_STR);
+            $stmtCreate->bindParam(':password', $password, PDO::PARAM_STR);
 
-            $statement->execute();
-            $statement = $pdo->prepare('SELECT * FROM users WHERE email = :email');
-            // bind the parameter to the if(isset) so it's exists
-            $statement->bindParam(':email', $email);
-            // execute the code
-            $statement->execute();
-            // fecth the data from the database, fetch_assoc get a clean output
-            $user = $statement->fetch(PDO::FETCH_ASSOC);
+            $stmtCreate->execute();
+
+            //Fetch user data and set session
+            $stmtGetInfo = $pdo->prepare('SELECT * FROM users WHERE email = :email');
+            $stmtGetInfo->bindParam(':email', $email);
+
+            $stmtGetInfo->execute();
+
+            $user = $stmtGetInfo->fetch(PDO::FETCH_ASSOC);
             $_SESSION['user'] = $user;
             redirect();        
         }
