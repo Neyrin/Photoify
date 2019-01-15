8b7e1b207a9dc2bf3c3e37e83bf5f894b87ead92
Neyrin <isa.areschoug@gmail.com>
2019-01-15 21:35:53 +0100

    Added commentary

--- app/posts/edit/edit-post.php
+++ app/posts/edit/edit-post.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 require __DIR__.'/../../autoload.php';
 
-
+//Get new caption and post id from form
+//Then add caption to matching id in db
 if(isset($_POST['caption'], $_POST['selected-post-id'])) {
     $newCaption = trim(filter_var($_POST['caption'], FILTER_SANITIZE_STRING)) ?? ' ';
     $selectedPostId = filter_var($_POST['selected-post-id'], FILTER_SANITIZE_NUMBER_INT);
