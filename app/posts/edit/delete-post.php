b8b31f54de8cbac01c973c724acca01cbb0d066f
Neyrin <isa.areschoug@gmail.com>
2019-01-15 21:15:18 +0100

    Added delete logic

---
+++ app/posts/edit/delete-post.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+require __DIR__.'/../../autoload.php';
+
+
+if(isset($_POST['selected-post-id'])) {
+    $selectedPostId = filter_var($_POST['selected-post-id'], FILTER_SANITIZE_NUMBER_INT);
+
+
+    $stmtDelete = $pdo->prepare('DELETE FROM Posts WHERE post_id = :post_id');
+    $stmtDelete->bindParam(':post_id', $selectedPostId, PDO::PARAM_INT);
+    if (!$stmtDelete) {
+        die(var_dump($pdo->errorInfo()));
+    }
+    $stmtDelete->execute();
+} 
+redirect(); 
