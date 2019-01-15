7e51f0d92607791ad7a2328bd2b88a843ddfe889
Neyrin <isa.areschoug@gmail.com>
2019-01-15 21:41:36 +0100

    Cleanup

--- app/users/feed2.php
+++ app/users/feed2.php
@@ -2,19 +2,6 @@
 declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
-/* //Retrieve data by newest first
-$stmtPosts = $pdo->query('SELECT * FROM Posts ORDER BY date DESC');
-$stmtPosts->execute();
-
-if (!$stmtPosts) {
-    die(var_dump($pdo->errorInfo()));
-}
-
-$posts = $stmtPosts->fetchAll(PDO::FETCH_ASSOC);
-
-header('Content-Type: application/json');
-echo json_encode($posts); 
- */
 
 $stmtPosts = $pdo->prepare(
     'SELECT p.image, p.caption, p.post_id, u.user_name, u.avatar, u.user_id, count(a.like)
