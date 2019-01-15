8cc75386f7d7fc81d850f1d2e79719d95bd98646
Neyrin <isa.areschoug@gmail.com>
2019-01-15 20:35:23 +0100

    Created logic to edit post

--- edit.php
+++ edit.php
@@ -1,11 +1,23 @@
 <?php require __DIR__.'/views/header.php'; ?>
-<?php checkForLoggedInUser();?>
+<?php checkForLoggedInUser();
 
+if(isset($_POST['post-user-id'], $_POST['post-id'])) {
+    if($_POST['post-user-id'] !== $user_id) {
+        $_SESSION['messages'][] = "You don't have permission to edit this post";
+    redirect('/');
+    } else {
+        $selected_post_id = filter_var($_POST['post-id'], FILTER_SANITIZE_NUMBER_INT);
+        print_r($selected_post_id);
+    }
+} else {$_SESSION['messages'][] = "There is no post to edit.";
+redirect('/');
+}
+?>
 
-
-<img class="post-to-edit">
-<p class="caption-to-edit"></p>
+<!-- <img class="post-to-edit">
+<p class="caption-to-edit"></p> -->
 <form class="edit-post" action="/app/posts/edit/edit-post.php" method="post">
+    <input type="hidden" name="selected-post-id" value=<?= $selected_post_id ?>>
     <input class="form-upload" type="text" name="caption" placeholder="UPDATE YOUR CAPTION?">
     <button class="edit-post-btn" type="submit" name="edit-post-btn">EDIT</button>
 </form>
