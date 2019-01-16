5521ed9c2903a00cf20854741f4090c78530d23e
Neyrin <isa.areschoug@gmail.com>
2019-01-16 22:59:44 +0100

    Added header with logo

--- views/header.php
+++ views/header.php
@@ -15,18 +15,28 @@ require __DIR__.'/../app/autoload.php';
     <link rel="stylesheet" href="assets/style/upload.css">
     <link rel="stylesheet" href="assets/style/feed.css">
     <link rel="stylesheet" href="assets/style/mypage.css">
+    <link rel="stylesheet" href="assets/style/settings.css">
     <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
 <body>
 <?php 
 displayErrorMessage();
 ?>
+<header>
+	<img class="logo" src="assets/style/images/Photoify-logo-white.png">  
+</header>
+
     <div class="navbar">
-      <?php if(isset($_SESSION['user'])): ?>
-      <button class="btn" id="home_btn"><i class="fa fa-home" aria-hidden="true"></i></button>
-      <button class="btn" id="upload_btn"><i class="fa fa-camera-retro"></i></button>
-      <button class="btn" id="user_btn"><i class="fa fa-user-circle-o" aria-hidden="true"></i></button> 
-    <?php
+		<div class="logo-container">
+			<img class="logo" src="assets/style/images/Photoify-logo-white.png">
+		</div>
+<?php if(isset($_SESSION['user'])): ?>
+	<div class="buttons">
+		<button class="btn" id="home_btn"><i class="fa fa-home" aria-hidden="true"></i></button>
+		<button class="btn" id="upload_btn"><i class="fa fa-camera-retro"></i></button>
+		<button class="btn" id="user_btn"><i class="fa fa-user-circle-o" aria-hidden="true"></i></button> 
+	</div>	
+	<?php
     else: ?> 
     <?php endif; ?>
     </div>
