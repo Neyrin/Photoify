b34c8669efa0297decaa783b1d243618333e032d
Isa Areschoug <42944657+Neyrin@users.noreply.github.com>
2019-01-17 22:49:01 +0100

    Removed redundant code

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php require __DIR__.'/views/header.php';?>
 
-<article>
-    <?php
-    /* If there's an active user redirect to home */
-    if(isset($_SESSION['user'])):  ?>
-    <?php require __DIR__.'/home.php'; ?>
 
-    <?php
-    /* If there's no active user redirect to start/login page */
-    else: ?>
-        <?php require __DIR__.'/start.php'; ?>
-    <?php endif; ?>
-</article>
+<?php
+/* If there's an active user redirect to home */
+if(isset($_SESSION['user'])):  ?>
+<?php require __DIR__.'/home.php'; ?>
+
+<?php
+/* If there's no active user redirect to start/login page */
+else: ?>
+    <?php require __DIR__.'/start.php'; ?>
+<?php endif; ?>
 
 
 <?php require __DIR__.'/views/footer.php'; ?>
