5ec0ca1dd224147f5bfb97cf90afffe36deabb8b
Isa Areschoug <42944657+Neyrin@users.noreply.github.com>
2019-01-17 22:53:25 +0100

    Made more readable

--- app/users/feed.php
+++ app/users/feed.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 
 $stmtPosts = $pdo->prepare(
-    'SELECT p.image, p.caption, p.post_id, u.user_name, u.avatar, u.user_id, count(a.likes)
+    'SELECT p.image, 
+    p.caption, 
+    p.post_id, 
+    u.user_name, 
+    u.avatar, 
+    u.user_id, 
+    count(a.likes)
+ 
     AS "likes"
     FROM Posts p
     LEFT JOIN Users u ON p.user_id=u.user_id
@@ -16,6 +23,10 @@ $stmtPosts = $pdo->prepare(
     }
     $stmtPosts->execute();
     $posts = $stmtPosts->fetchAll(PDO::FETCH_ASSOC);
+
+    for ($i=0; $i < count($posts); $i++) { 
+        $posts[$i]['logged_in_user'] = $user_id;
+    }
 
 header('Content-Type: application/json');
 echo json_encode($posts);
